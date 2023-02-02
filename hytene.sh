3d54b00bd05e4cc27974ed668dd2dc1ef592112c
johnnymatthews <9611008+johnnymatthews@users.noreply.github.com>
2023-02-02 16:53:44 -0400

    Adds basic hytene usage.

--- hytene.sh
+++ hytene.sh
@@ -14,6 +14,22 @@ fi
 create_daemon () {
     echo "Creating daemon..."
 
+    # Check if running as root.
+    if (( $EUID != 0 )); then
+        echo "ERROR: you must run this function as root."
+        exit
+    fi
+
+    # Install dependencies.
+    sudo apt update -y
+    sudo apt install mesa-opencl-icd ocl-icd-opencl-dev gcc git bzr jq pkg-config curl clang build-essential hwloc libhwloc-dev wget -y
+
+    # Clone, checkout, and build Lotus
+    git clone https://github.com/filecoin-project/lotus.git
+    cd lotus/
+    git checkout ntwk/hyperspace
+    make clean && make hyperspacenet
+    sudo make install
 
     echo "Daemon created and ready."
 }
@@ -21,6 +37,8 @@ create_daemon () {
 start_daemon () {
     echo "Starting daemon..."
 
+    LOTUS_API_LISTENADDRESS="/ip4/127.0.0.1/tcp/1234/http"
+    FULLNODE_API_INFO=wss://wss.hyperspace.node.glif.io/apigw/lotus lotus daemon --lite
  
     echo "Daemon stopped."
 }
@@ -54,8 +72,8 @@ show_help () {
     echo "./hytene.sh <FUNCTION>"
     echo " "
     echo "FUNCTIONS:"
-    echo "create_daemon - creates the Filecoin daemon."
-    echo "start_daemon - starts the daemon. createDaemon must be ran before this."
+    echo "create_daemon - creates the lite-node daemon. Requires root."
+    echo "start_daemon - starts the lite-node daemon. create_daemon must be ran before this."
     echo "delete_everything: deletes everything Lotus related. Requires root."
     echo " "
     echo "EXAMPLE:"
